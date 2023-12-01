75c156df618458d1c4ae81928aa352327e8f0319
kyawmiu <95058916+kyawmiu@users.noreply.github.com>
2023-12-01 15:47:05 -0600

    Time: 5 ms (54.70%), Space: 42.3 MB (73.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,24 +1,18 @@
 class Solution {
    public  boolean isAnagram(String s, String t) {
-    HashMap<Character,Integer> map1= consists(s);
-    HashMap<Character,Integer> map2= consists(t);
+     if (s.length() != t.length()) return false;
 
-	return map1.equals(map2);
+    int[] store = new int[26];
+
+    for (int i = 0; i < s.length(); i++) {
+        store[s.charAt(i) - 'a']++;
+        store[t.charAt(i) - 'a']--;
     }
 
-public  HashMap<Character,Integer> consists(String a){
-	
-	HashMap<Character,Integer> map= new HashMap<>();
-	for (int i = 0; i < a.length(); i++) {
-		if(map.containsKey(a.charAt(i))==true) {
-			int temp=map.get(a.charAt(i))+1;
-			map.put(a.charAt(i), temp);
-		}else {
-			map.put(a.charAt(i), 1);
-		}
-		
-		
-	}
-	return map;
-}
+    for (int n : store) if (n != 0) return false;
+
+    return true;
+    }
+
+
 }
